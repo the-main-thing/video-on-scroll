af9bfb3e96b5c1ec661c7da75d8a94df9209604b
Pavel Shevtsov <mail@pavelshevtsov.com>
2022-11-18 09:25:17 +0300

    Proper logic for scrolling size

--- src/index.ts
+++ src/index.ts
@@ -2,68 +2,61 @@ export type OnRange = (data: {
 	start: number
 	end: number
 	current: number
-	inRange: typeof inRange
-	traveledInRange: typeof traveledInRange
+	getPosition: (start: number, end: number) => ReturnType<typeof inRange>
 }) => void
 
-export async function videoOnScroll({
-	video: videoSelector,
+// videoOnScroll({
+// 	url: 'https://tilda.ws/video_test/drawing_4_1_3.mp4',
+// 	scrollSize: 2000,
+// 	eventsOnScroll: [
+// 		({ current, getPosition }) => {
+// 			console.log(current)
+// 			console.log(...getPosition(100, 200))
+// 		},
+// 	],
+// })
+
+export default async function videoOnScroll({
+	url,
 	scrollSize,
 	eventsOnTime,
 	eventsOnScroll,
 }: {
-	video: string | HTMLVideoElement
+	url: string | URL
 	scrollSize: number
 	eventsOnTime?: Array<OnRange>
 	eventsOnScroll?: Array<OnRange>
 }) {
-	const video =
-		typeof videoSelector === 'string'
-			? (document.querySelector(videoSelector) as HTMLVideoElement)
-			: videoSelector
-	if (!video) {
-		if (typeof videoSelector === 'string') {
-			throw new Error(
-				`Could not find video element using selector: "${videoSelector}"`
-			)
-		}
-		throw new Error(
-			'No video passed. Should pass video as a css selector or a HTMLElement'
-		)
-	}
-
-	if (video.nodeName.toLowerCase() !== 'video') {
-		if (typeof videoSelector === 'string') {
-			throw new Error(
-				`Element found using css selector "${videoSelector}" is not a \`<video>\` HTML tag.`
-			)
-		}
-		throw new Error('Element passed as video is not an HTMLVideoElement')
-	}
-
-	// Set up video attributes
-	video.setAttribute('tabindex', '0')
-	video.setAttribute('autobuffer', '')
-	video.setAttribute('preload', '')
-	video.style.position = 'fixed'
-
-	// Create height setter
-	const heightSetter = document.createElement('div')
-	heightSetter.style.display = 'block'
-	document.body.appendChild(heightSetter)
-	const windowHeight = window.innerHeight
-	const scrollHeigth = windowHeight + scrollSize
-	heightSetter.style.height = scrollHeigth + 'px'
-	const startingPosition = heightSetter.getBoundingClientRect().top
+	const heightSetter = createHeigthSetter(scrollSize)
+	const container = createContainer()
+	container.append(heightSetter)
+	const video = createVideo(url)
 
 	await waitForLoad(video)
+	mount(container, video)
+	window.scrollTo(0, 0)
 
-	let lastPosition = startingPosition
-	const playbackSpeed = Math.floor(scrollSize / video.duration)
+	const heightSetterRect = heightSetter.getBoundingClientRect()
+	const pxToTime = getPxToTime(video.duration, heightSetterRect.height)
+	let lastPosition = window.scrollY
 	const scrollPlay = () => {
-		const scrolled =
-			startingPosition - heightSetter.getBoundingClientRect().top
-		const frameNumber = window.pageYOffset / playbackSpeed
+		const scrolled = window.scrollY
+		if (lastPosition === scrolled) {
+			return window.requestAnimationFrame(scrollPlay)
+		}
+
+		const frameNumber = pxToTime(scrolled)
+		if (frameNumber >= video.duration) {
+			onEnd(video, scrolled)
+			return window.requestAnimationFrame(scrollPlay)
+		}
+
+		if (frameNumber < 0) {
+			return window.requestAnimationFrame(scrollPlay)
+		}
+
+		onStart(video)
+
 		video.currentTime = frameNumber
 		if (lastPosition !== scrolled) {
 			if (eventsOnScroll?.length) {
@@ -71,8 +64,8 @@ export async function videoOnScroll({
 					start: 0,
 					end: scrollSize,
 					current: scrolled,
-					inRange,
-					traveledInRange,
+					getPosition: (start: number, end: number) =>
+						inRange(start, end, scrolled),
 				}
 				for (let i = 0; i < eventsOnScroll.length; i++) {
 					const onScrollHandler = eventsOnScroll[i]
@@ -85,8 +78,8 @@ export async function videoOnScroll({
 					start: 0,
 					end: video.duration,
 					current: frameNumber,
-					inRange,
-					traveledInRange,
+					getPosition: (start: number, end: number) =>
+						inRange(start, end, frameNumber),
 				}
 				for (let i = 0; i < eventsOnTime.length; i++) {
 					const onTimeHandler = eventsOnTime[i]
@@ -95,14 +88,34 @@ export async function videoOnScroll({
 			}
 		}
 		lastPosition = scrolled
-		window.requestAnimationFrame(scrollPlay)
+		return window.requestAnimationFrame(scrollPlay)
 	}
 
 	window.requestAnimationFrame(scrollPlay)
 }
 
-function inRange(start: number, end: number, current: number): boolean {
-	return current >= start && current <= end
+function getPxToTime(duration: number, scrollLength: number) {
+	const playbackSpeed = duration / (scrollLength - window.innerHeight)
+	console.log({
+		scrollLength,
+		duration,
+		playbackSpeed,
+	})
+	return (scrolled: number) => {
+		console.log(scrolled, scrolled * playbackSpeed)
+		return scrolled * playbackSpeed
+	}
+}
+
+function inRange(
+	start: number,
+	end: number,
+	current: number
+): [inRange: boolean, traveledWithinRange: number] {
+	return [
+		current >= start && current <= end,
+		traveledInRange(start, end, current),
+	]
 }
 
 function traveledInRange(start: number, end: number, current: number): number {
@@ -111,42 +124,124 @@ function traveledInRange(start: number, end: number, current: number): number {
 	return (rangeSize / 100) * currentPositionInRange
 }
 
-async function waitForLoad(video: HTMLVideoElement): Promise<void> {
-	await Promise.all([
-		new Promise<void>(resolve => {
-			let loadedCounter = 0
-			const addEventListener = (
-				eventName: keyof HTMLVideoElementEventMap
-			) => {
-				loadedCounter += 1
-				const callback = () => {
-					loadedCounter -= 1
-					if (loadedCounter === 0) {
-						window.removeEventListener(eventName, callback)
-						return resolve()
-					}
-				}
-				video.addEventListener(eventName, callback)
-			}
-
-			if (video.readyState > 2) {
+function waitForLoad(video: HTMLVideoElement): Promise<void> {
+	let loadedCounter = 0
+	const addEventListener = (
+		eventName: keyof HTMLVideoElementEventMap,
+		resolve: () => void
+	) => {
+		loadedCounter += 1
+		const callback = () => {
+			loadedCounter -= 1
+			if (loadedCounter === 0) {
+				video.removeEventListener(eventName, callback)
 				return resolve()
 			}
-			addEventListener('loadedmetadata')
-		}),
-		loadVideo(video),
-	])
+		}
+		video.addEventListener(eventName, callback)
+	}
+
+	return new Promise<void>(resolve => {
+		video.load()
+		if (video.readyState > 2) {
+			return resolve()
+		}
+		addEventListener('loadedmetadata', resolve)
+		addEventListener('loadeddata', resolve)
+	})
 }
 
-function loadVideo(video: HTMLVideoElement): Promise<void> {
-	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest()
-		const url = new URL(video.src)
-		xhr.open('GET', url, true)
-		xhr.responseType = 'arraybuffer'
-		xhr.onloadend = () => resolve()
-		xhr.onerror = () =>
-			reject(new Error(`Error loading video: ${xhr.status}`))
-		xhr.send()
-	})
+function getVideoWidth(video: HTMLVideoElement): `${number}px` {
+	const videoRatio = video.videoWidth / video.videoHeight
+	const screenRatio = window.innerWidth / window.innerHeight
+	const elementsRatio = videoRatio / screenRatio
+	const width =
+		elementsRatio >= 1 ? window.innerHeight * videoRatio : window.innerWidth
+	return `${Math.floor(width)}px`
+}
+
+function getVideoStyles(video: HTMLVideoElement) {
+	return {
+		display: 'block',
+		position: 'fixed',
+		top: '0',
+		left: '0',
+		width: getVideoWidth(video),
+	} as const
+}
+
+function setVideoStyles(video: HTMLVideoElement) {
+	const videoStyles = getVideoStyles(video)
+	for (const [key, value] of Object.entries(videoStyles)) {
+		video.style[key as keyof typeof videoStyles] = value
+	}
+}
+
+function mount(container: HTMLElement, video: HTMLVideoElement) {
+	const videoContainer = document.createElement('div')
+	videoContainer.style.display = 'none'
+	videoContainer.style.position = 'absolute'
+	videoContainer.style.top = '0'
+	videoContainer.style.left = '0'
+	videoContainer.style.overflow = 'hidden'
+	videoContainer.style.height = window.innerHeight + 'px'
+	videoContainer.style.width = window.innerWidth + 'px'
+	videoContainer.appendChild(video)
+	container.appendChild(videoContainer)
+	document.body.prepend(container)
+	setVideoStyles(video)
+	videoContainer.style.display = 'block'
+}
+
+function createVideo(url: string | URL): HTMLVideoElement {
+	const video = document.createElement('video')
+	const source = document.createElement('source')
+	source.src = new URL(url, window.location.href).toString()
+	video.src = source.src
+	video.appendChild(source)
+
+	// Set up video attributes
+	video.setAttribute('tabindex', '0')
+	video.setAttribute('autobuffer', '')
+	video.muted = true
+	video.controls = false
+	video.preload = 'auto'
+
+	return video
+}
+
+function createHeigthSetter(scrollSize: number): HTMLDivElement {
+	const heightSetter = document.createElement('div')
+	heightSetter.style.height = window.innerHeight * 2 + scrollSize + 'px'
+	return heightSetter
+}
+
+function createContainer(): HTMLDivElement {
+	const container = document.createElement('div')
+	container.style.position = 'relative'
+	return container
+}
+
+function onEnd(video: HTMLVideoElement, scrolled: number) {
+	const parent = video.parentNode as HTMLDivElement
+	if (parent.style.position === 'fixed') {
+		parent.style.position = 'absolute'
+		parent.style.top = scrolled + 'px'
+		parent.style.backgroundColor = 'red'
+		parent.style.display = 'flex'
+		parent.style.alignItems = 'bottom'
+		if (video.style.position === 'fixed') {
+			video.style.position = 'absolute'
+		}
+	}
+}
+
+function onStart(video: HTMLVideoElement) {
+	const parent = video.parentNode as HTMLDivElement
+	if (parent.style.position !== 'fixed') {
+		parent.style.position = 'fixed'
+		parent.style.top = '0'
+		parent.style.float = 'none'
+		setVideoStyles(video)
+	}
 }
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,17 +1,18 @@
-import { videoOnScroll } from '.'
+import videoOnScroll from '.'
 import './style.css'
 
 videoOnScroll({
-	video: '#video',
-	scrollSize: 1000,
-	eventsOnScroll: [
-		({ current, inRange, traveledInRange }) => {
-			if (inRange(100, 200, current)) {
-				console.log('current', {
-					current,
-					traveled: traveledInRange(100, 200, current),
-				})
-			}
-		},
-	],
+	url: '/test.mp4',
+	scrollSize: 400,
+	// eventsOnScroll: [
+	// 	({ current, getPosition }) => {
+	// 		const [inRange, traveled] = getPosition(100, 200)
+	// 		if (inRange) {
+	// 			console.log('current', {
+	// 				current,
+	// 				traveled,
+	// 			})
+	// 		}
+	// 	},
+	// ],
 })
